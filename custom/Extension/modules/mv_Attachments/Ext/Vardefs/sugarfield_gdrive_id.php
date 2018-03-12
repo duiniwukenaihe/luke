d0f0dddc276f52a5a077c0f80e20f3b1ca8a923b
Imran Kabir <imran.kabir@rolustech.net>
2018-03-12 19:28:46 +0500

    added gdrive_id field to mv_attachments module

---
+++ custom/Extension/modules/mv_Attachments/Ext/Vardefs/sugarfield_gdrive_id.php
@@ -0,0 +1,11 @@
+<?php
+$dictionary["mv_Attachments"]["fields"]["gdrive_id"] = array(
+    'name' => 'gdrive_id',
+    'vname' => 'LBL_GDRIVE_ID',
+    'type' => 'varchar',
+    'len' => 300,
+    'reportable' => false,
+    'massupdate' => false,
+    'importable' => 'false',
+    'studio' => false,
+);
